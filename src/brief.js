6cae493f26313b28639e09affc5858eb48bb437a
jeffshaver <jeff.e.shaver@gmail.com>
2014-01-23 22:15:01 -0500

    Now JSLint and JSHint compliant

--- src/brief.js
+++ src/brief.js
@@ -1,41 +1,43 @@
-var $ = function(query) {
-  var r = document.querySelectorAll(query);
-  return r.length == 1 ? r[0] : r;
-};
-Element.prototype.on = function(type, callback, delegatee) {
-  if (!delegatee) {
-    this.addEventListener(type, callback, false);
-  } else {
-    var elements = $(delegatee);
-    if (!elements.length) {
-      this.addEventListener(type, function(event) {
-        if (event.srcElement.id == delegatee.substring(1)) {
-          callback(event);
-        }
-      }, true);
+(function (document, Element, NodeList) {
+  'use strict';
+  var $ = function (query) {
+    var r = document.querySelectorAll(query);
+    return r.length === 1 ? r[0] : r;
+  };
+  Element.prototype.on = function (type, callback, delegatee) {
+    if (!delegatee) {
+      this.addEventListener(type, callback, false);
     } else {
-      console.log(delegatee.indexOf('.'));
-      if (delegatee.indexOf('.') == -1) {
-        this.addEventListener(type, function(event) {
-            if (event.srcElement.tagName.toLowerCase() == (delegatee)) {
-                callback(event);
-            }
+      var elements = $(delegatee);
+      if (!elements.length) {
+        this.addEventListener(type, function (event) {
+          if (event.srcElement.id === delegatee.substring(1)) {
+            callback(event);
+          }
         }, true);
       } else {
-        this.addEventListener(type, function(event) {
-            if (event.srcElement.classList.contains(delegatee.substring(1))) {
-                callback(event);
+        if (delegatee.indexOf('.') === -1) {
+          this.addEventListener(type, function (event) {
+            if (event.srcElement.tagName.toLowerCase() === delegatee) {
+              callback(event);
             }
-        }, true);
+          }, true);
+        } else {
+          this.addEventListener(type, function (event) {
+            if (event.srcElement.classList.contains(delegatee.substring(1))) {
+              callback(event);
+            }
+          }, true);
+        }
       }
     }
-  }
-  return this;
-}
+    return this;
+  };
 
-NodeList.prototype.on = function(type, callback, delegatee) {
-  [].forEach.call(this, function(element) {
-    element.on(type, callback, delegatee);
-  });
-  return this;
-}
+  NodeList.prototype.on = function (type, callback, delegatee) {
+    [].forEach.call(this, function (element) {
+      element.on(type, callback, delegatee);
+    });
+    return this;
+  };
+}(document, Element, NodeList));
