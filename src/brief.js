9620635c8bd93bc635e18b8496ddc298a31cdbbf
jeffshaver <jeff.e.shaver@gmail.com>
2014-02-16 03:59:53 -0500

    Update how we handle all listeners so that we don't apply a listener to every element

--- src/brief.js
+++ src/brief.js
@@ -12,13 +12,12 @@
  * see https://github.com/umdjs/umd/blob/master/returnExports.js
  */
 (function(root, factory) {
-  'use strict';
-  if (typeof define === 'function' && define.amd) {
+  if (typeof define == 'function' && define.amd) {
     /*
      * AMD. Register as an anonymous module.
      */
     define([], factory);
-  } else if (typeof exports === 'object') {
+  } else if (typeof exports == 'object') {
     /*
      * Node. Does not work with strict CommonJS, but
      * only CommonJS-like enviroments that support module.exports,
@@ -42,7 +41,9 @@
   /*
    * For delegated listeners, we will need to manage a list of callbacks
    */
+  var delegatedListeners = {};
   var managedListeners = {};
+  var managedElements = {};
   /*
    * We need to save references to some prototype methods that
    * we will need later
@@ -101,24 +102,33 @@
     return newBrief;
   };
   var delegatedListener = function(event) {
-    var srcElement = event.srcElement;
-    var listeners = managedListeners[event.type];
+    var target = event.target;
+    var listeners = delegatedListeners[event.type];
     var i;
     var listener;
     var element;
     var delegatedTo;
-    if (srcElement == document) {
+    if (target === document) {
       return;
     }
     for (i = 0; i < listeners.length; i++) {
       listener = listeners[i];
       element = listener.element;
       delegatedTo = listener.delegatedTo;
-      if (match(srcElement, delegatedTo)) {
-        if (element.contains(srcElement)) {
+      if (match(target, delegatedTo)) {
+        if (element.contains(target)) {
           listener.callback.call(this, event);
         }
       }
+    }
+  };
+  var managedListener = function(event) {
+    var elements = managedElements[event.type];
+    var listeners = managedListeners[event.type];
+    var index = elements.indexOf(event.target);
+    while (index !== -1) {
+      listeners[index].call(this, event);
+      index = elements.indexOf(event.target, index+1);
     }
   };
   /*
@@ -307,7 +317,13 @@
            * can just apply the listener to the element
            */
           if (!delegatee) {
-            element.addEventListener(type, newFunction, false);
+            if (!managedElements[type]) {
+              managedElements[type] = [];
+              managedListeners[type] = [];
+              document.addEventListener(type, managedListener, true);
+            }
+            managedElements[type].push(element);
+            managedListeners[type].push(newFunction);
             continue;
           }
 
@@ -318,15 +334,15 @@
            * to handle all the events
            */
 
-          if (!managedListeners[type]) {
-            managedListeners[type] = [];
+          if (!delegatedListeners[type]) {
+            delegatedListeners[type] = [];
             document.addEventListener(type, delegatedListener, true);
           }
           /*
            * We need to push an instance of this type into
            * the array so we can invoke/remove it later
            */
-          managedListeners[type].push({
+          delegatedListeners[type].push({
             element: element,
             delegatedTo: delegatee,
             callback: callback
@@ -336,7 +352,7 @@
     },
     off: function(types, callback, delegatee) {
       var me = this;
-      var type, element, len, listeners, listener, i, j;
+      var type, elements, element, len, listeners, listener, index, i, j;
       /*
        * Since we want to support multiple listeners types
        * at once, we need to split up the types if they
@@ -367,14 +383,36 @@
            * just remove the listener from the element
            */
           if (!delegatee) {
-            element.removeEventListener(type, callback, false);
+            listeners = managedListeners[type];
+            elements = managedElements[type];
+            index = listeners.indexOf(callback);
+            /*
+             * While we can still find a match 
+             * in the listeners array, keep splicing
+             */
+            while(index !== -1) {
+              if (element === elements[index]) {
+                elements.splice(index, 1);
+                listeners.splice(index, 1);
+                /*
+                 * Since we spliced out an item, we need to
+                 * decrease our current position so we can
+                 * correctly calculate the next index
+                 */
+                index--;
+              }
+              /*
+               * We can now look for the next index (if any)
+               */
+              index = listeners.indexOf(callback, index+1);
+            }
             continue;
           }
           /*
            * If we are dealing with a delegated listener,
            * we need to get the listeners for this type
            */
-          listeners = managedListeners[type];
+          listeners = delegatedListeners[type];
           len = listeners.length;
           /*
            * Loop through the listeners for this type
@@ -383,9 +421,9 @@
            */
           while (len--) {
             listener = listeners[len];
-            if (listener.element == element &&
-                listener.delegatedTo == delegatee &&
-                listener.callback == callback) {
+            if (listener.element === element &&
+                listener.delegatedTo === delegatee &&
+                listener.callback === callback) {
               listeners.splice(len, 1);
             }
           }
@@ -432,7 +470,7 @@
       /*
        * If we just grabbed the elements related to the context
        */
-      if (this.selector == context) {
+      if (this.selector === context) {
         this.find(selector);
       /*
        * If we are dealing with a context which is a brief object
